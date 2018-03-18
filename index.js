b1b91a3cf1ed0f4c2e0faefd8f095ff46d66c0d4
msahil432 <sahil_432@yahoo.in>
2018-03-18 12:25:55 +0530

    trial 2

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 var express = require('express')
 var app = express()
+const path = require('path')
+const http = require('http')
 
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/dist'))
 
-app.get('/', function(request, response) {
-  response.send('To be implemented!')
+app.get('*', function(request, response) {
+  response.sendFile(path.join(__dirname, 'dist/index.html'))
 })
 
 app.get('/r', function(request, response) {
     response.send('Redirects, To be implemented soon!')
 })
 
-app.listen(app.get('port'), function() {
+const server = http.createServer(app)
+server.listen(app.get('port'), function() {
   console.log("App is running at localhost:" + app.get('port'))
 })
