47e327de05cdef2de05e8231ebff0e98823c4846
msahil432 <sahil82432malik@live.com>
2018-06-07 18:14:28 +0530

    changing profile - trying iterators

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Globals} from '../../globals';
+
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css']
+  styleUrls: ['./user-profile.component.css'],
+  providers: [Globals]
 })
 export class UserProfileComponent implements OnInit {
 
--- src/globals.ts
+++ src/globals.ts
@@ -11,4 +11,14 @@ export class Globals {
                     {name:"LinkedIn", link:"https://linkedin.com/in/msahil432"},
                     {name:"Twitter",  link:"https://twitter.com/msahil432"}
                   ];
+  public myExp : Array<{company: string, position: string, details: string}> = 
+                  [
+                    {
+                      company: "MyMovieRack.com", position: "Software Intern", 
+                      details:"6 Weeks <i>Paid</i> Internship in July 2017. Worked on two things, First comprised of a Java Servlet built using Jetty and finally deployed on AWS Instance. Second was a chrome Extension employing Bootstrap, Jquery but it was not published."
+                    }
+                  ];
+  public myEdu : Array<{institute: string, degree: string, details: string}> = 
+                  [];
+  
 }
