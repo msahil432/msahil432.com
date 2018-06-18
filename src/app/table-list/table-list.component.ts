9f1e6fd87e794f88ccf48ecfb834d18f2d18f09b
msahil432 <sahil82432malik@live.com>
2018-06-18 21:45:34 +0530

    projects start

--- src/app/table-list/table-list.component.ts
+++ src/app/table-list/table-list.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Globals} from '../../globals';
+
 @Component({
   selector: 'app-table-list',
   templateUrl: './table-list.component.html',
-  styleUrls: ['./table-list.component.css']
+  styleUrls: ['./table-list.component.css'],
+  providers : [Globals]
 })
 export class TableListComponent implements OnInit {
 
-  constructor() { }
+  constructor(public globals: Globals) { }
 
   ngOnInit() {
   }
