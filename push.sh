f80cab5c9f08203abcfa67470a547b8c92f8920a
msahil432 <sahil82432malik@live.com>
2018-06-19 16:25:27 +0530

    modules updated

--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@
 #Made by msahil432
 RED='\033[0;31m'
 
-echo -e "$(RED)-----------       git add .                          --------------"
+echo -e "${RED}-----------       git add .                          --------------"
 git add .
 
 echo "-----------       git commit - $1  --------------"
