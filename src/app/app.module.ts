0e957803424d17cf77fa3389b363fb9c31a049d4
msahil432 <sahil82432malik@live.com>
2018-06-22 16:48:14 +0530

    updating to latest - app.modules.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,15 @@ import { ComponentsModule } from './components/components.module';
 import { AppComponent } from './app.component';
 
 import {AgmCoreModule} from '@agm/core';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { TableListComponent } from './table-list/table-list.component';
+import { TypographyComponent } from './typography/typography.component';
+import { IconsComponent } from './icons/icons.component';
+import { MapsComponent } from './maps/maps.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { UpgradeComponent } from './upgrade/upgrade.component';
   
 @NgModule({
       imports: [
@@ -20,11 +29,19 @@ import {AgmCoreModule} from '@agm/core';
         RouterModule,
         AppRoutingModule,
         AgmCoreModule.forRoot({
-          apiKey: 'YOUR_GOOGLE_MAPS_API_KEY'
+          apiKey: 'AIzaSyAtRsBCSj7ftvdK9tglMRgKK3-S5dH-o0g'
         })
       ],
   declarations: [
     AppComponent,
+    DashboardComponent,
+    UserProfileComponent,
+    TableListComponent,
+    TypographyComponent,
+    IconsComponent,
+    MapsComponent,
+    NotificationsComponent,
+    UpgradeComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
