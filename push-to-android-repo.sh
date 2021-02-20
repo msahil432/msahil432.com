eeac2a4c0724e3ae4171fe1378dae6ea9f4835ac
msahil432 <dev@msahil432.com>
2021-02-20 11:49:02 +0530

    Remove Android APP code

---
+++ push-to-android-repo.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+set -e  # if a command fails it stops the execution
+set -u  # script fails if trying to access to an undefined variable
+
+export API_TOKEN_GITHUB=$1
+
+
+echo "Starts"
+SOURCE_DIRECTORY="dist"
+COMMIT_MESSAGE="See ORIGIN_COMMIT"
+
+CLONE_DIR=$(mktemp -d)
+
+echo "Cloning destination git repository"
+# Setup git
+git config --global user.email "dev@msahil432.com"
+git config --global user.name "msahil432"
+git clone --single-branch --branch "master" "https://$API_TOKEN_GITHUB@github.com/msahil432/com.msahil432.profile.git" "$CLONE_DIR"
+ls -la "$CLONE_DIR"
+
+echo "Copying contents to git repo"
+rm -rf "$CLONE_DIR"/www
+cp -ra "$SOURCE_DIRECTORY"/. "$CLONE_DIR"/www
+cd "$CLONE_DIR"
+
+echo "Updating things for Cordova"
+./prepare-for-cordova.sh
+
+echo "Files that will be pushed"
+ls -la
+
+echo "Adding git commit"
+
+ORIGIN_COMMIT="https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
+COMMIT_MESSAGE="${COMMIT_MESSAGE/ORIGIN_COMMIT/$ORIGIN_COMMIT}"
+
+git add .
+git status
+
+# git diff-index : to avoid doing the git commit failing if there are no changes to be commit
+git diff-index --quiet HEAD || git commit --message "$COMMIT_MESSAGE"
+
+echo "Pushing git commit"
+# --set-upstream: sets de branch when pushing to a branch that does not exist
+git push origin --set-upstream master
